afb7dc03f952640943eec79806282c0d3cfcd1d5
SuJiaKuan <jiakuan.su@toppano.in>
2016-07-28 16:06:26 +0800

    Enalbe auto play

--- src/LivePhotoPlayer.js
+++ src/LivePhotoPlayer.js
@@ -6,11 +6,13 @@ import inRange from 'lodash/inRange';
 import raf from 'raf';
 
 import { DIRECTION } from 'constants/common';
-import LIVEPHOTO_DEFAULT from 'constants/livephoto';
+import { LIVEPHOTO_DEFAULT, PLAY_MODE, AUTO_PLAY_DIR } from 'constants/livephoto';
 import EVENTS from 'constants/events';
 import { isMobile } from 'lib/devices';
 import { getPosition } from 'lib/events/click';
 import Gyro from './Gyro';
+
+const AUTO_PLAY_MAGIC_NUMBER = 100;
 
 export default class LivePhotoPlayer {
   constructor(params) {
@@ -24,12 +26,22 @@ export default class LivePhotoPlayer {
       this.direction === DIRECTION.HORIZONTAL ?
       (params.dimension.width * LIVEPHOTO_DEFAULT.SWIPE_RANGE) / this.numPhotos :
       (params.dimension.height * LIVEPHOTO_DEFAULT.SWIPE_RANGE) / this.numPhotos;
+    this.thresholdToAutoPlay =
+      Math.round(LIVEPHOTO_DEFAULT.MANUAL_TO_AUTO_THRESHOLD / AUTO_PLAY_MAGIC_NUMBER);
+    this.thresholdToManualPlay =
+      Math.round(LIVEPHOTO_DEFAULT.AUTO_TO_MANUAL_THRESHOLD / AUTO_PLAY_MAGIC_NUMBER);
 
     // Writable member variables
     this.photos = fill(Array(this.numPhotos), null);
     this.numLoadedPhotos = 0;
     this.isPlayerEnabled = false;
     this.curPhoto = -1;
+    this.playMode = PLAY_MODE.NONE;
+    this.autoPlayDir = AUTO_PLAY_DIR.STL;
+    this.isWaitManualToAuto = false;
+    this.isWaitAutoToManual = false;
+    this.countToAutoPlay = this.thresholdToAutoPlay;
+    this.countToManualPlay = this.thresholdToManualPlay;
     this.lastPixel = null;
     this.curPixel = null;
     this.lastRotation = null;
@@ -103,21 +115,79 @@ export default class LivePhotoPlayer {
       if (isMobile()) {
         new Gyro(this.handleRotation).start();
       }
+      this.playMode = PLAY_MODE.MANUAL;
       raf(this.onAnimationFrame);
     }
   }
 
+  startWaitManualToAuto() {
+    if (this.playMode === PLAY_MODE.MANUAL && !this.isWaitManualToAuto) {
+      this.isWaitManualToAuto = true;
+      this.countToAutoPlay = this.thresholdToAutoPlay;
+      setTimeout(() => {
+        if (this.countToAutoPlay > 0) {
+          this.playMode = PLAY_MODE.AUTO;
+        }
+        this.isWaitManualToAuto = false;
+      }, LIVEPHOTO_DEFAULT.MANUAL_TO_AUTO_THRESHOLD);
+    }
+  }
+
+  startWaitAutoToManual() {
+    if (this.playMode === PLAY_MODE.AUTO && !this.isWaitAutoToManual) {
+      this.isWaitAutoToManual = true;
+      this.countToManualPlay = this.thresholdToManualPlay;
+      setTimeout(() => {
+        if (this.countToManualPlay <= 0) {
+          this.playMode = PLAY_MODE.MANUAL;
+        }
+        this.isWaitAutoToManual = false;
+      }, LIVEPHOTO_DEFAULT.AUTO_TO_MANUAL_THRESHOLD);
+    }
+  }
+
   onAnimationFrame = () => {
-    const delta = this.getPixelDelta() + this.getRotationDelta();
+    const pixelIndexDelta = this.getPixelIndexDelta();
+    const rotationIndexDelta = this.getRotationIndexDelta();
+    const absIndexDelta = Math.abs(pixelIndexDelta) + Math.abs(rotationIndexDelta);
+    let indexDelta = 0;
+
+    if (this.playMode === PLAY_MODE.AUTO && LIVEPHOTO_DEFAULT.AUTO_PLAY_ENABLED) {
+      if (this.autoPlayDir === AUTO_PLAY_DIR.STL) {
+        if ((this.curPhoto === this.numPhotos - 1) || !this.photos[this.curPhoto]) {
+          indexDelta = -1;
+          this.autoPlayDir = AUTO_PLAY_DIR.LTS;
+        } else {
+          indexDelta = 1;
+        }
+      } else {
+        if ((this.curPhoto === 0) || !this.photos[this.curPhoto]) {
+          indexDelta = 1;
+          this.autoPlayDir = AUTO_PLAY_DIR.STL;
+        } else {
+          indexDelta = -1;
+        }
+      }
+      this.countToManualPlay -= absIndexDelta;
+      if (LIVEPHOTO_DEFAULT.AUTO_PLAY_ENABLED && !this.isWaitAutoToManual) {
+        this.startWaitAutoToManual();
+      }
+    } else if (this.playMode === PLAY_MODE.MANUAL) {
+      indexDelta = pixelIndexDelta + rotationIndexDelta;
+      this.countToAutoPlay -= absIndexDelta;
+      if (LIVEPHOTO_DEFAULT.AUTO_PLAY_ENABLED && !this.isWaitManualToAuto) {
+        this.startWaitManualToAuto();
+      }
+    }
     this.lastPixel = this.curPixel;
     this.lastRotation = this.curRotation;
-    if (delta !== 0) {
-      this.renderPhotoByDelta(delta);
+    if (indexDelta !== 0) {
+      this.renderPhotoByDelta(indexDelta);
     }
     raf(this.onAnimationFrame);
   }
 
-  getPixelDelta() {
+  getPixelIndexDelta() {
     let indexDelta = 0;
 
     if (this.lastPixel && this.curPixel) {
@@ -131,7 +201,7 @@ export default class LivePhotoPlayer {
     return indexDelta;
   }
 
-  getRotationDelta() {
+  getRotationIndexDelta() {
     let indexDelta = 0;
 
     if (this.lastRotation && this.curRotation) {
--- src/constants/livephoto.js
+++ src/constants/livephoto.js
@@ -1,8 +1,28 @@
-const LIVEPHOTO_DEFAULT = {
+/* eslint-disable max-len */
+
+export const LIVEPHOTO_DEFAULT = {
   SWIPE_RANGE: 0.8, // The maximum swipe (or mouse moving) range to play all photos
   ROTATION_RANGE: 50, // The maximum rotation angle (in degree) to play all photos
   PLAY_THRESHOLD: 0.5, // The threshold (proportion of loaded photos to all photos) to start playing
   CONCURRENT_LOADING_PHOTOS: 10, // The limit number of concurrently loading photos
+  AUTO_PLAY_ENABLED: true, // Enable auto play or not if the user "keep still" for a while
+  MANUAL_TO_AUTO_THRESHOLD: 1000, // The period (in milliseconds) to wait for changing from manual to auto mode
+  AUTO_TO_MANUAL_THRESHOLD: 100, // The period (milliseconds) to wait for changing from auto to manual mode
 };
 
-export default LIVEPHOTO_DEFAULT;
+export const PLAY_MODE = {
+  NONE: 'NONE',
+  MANUAL: 'MANUAL',
+  AUTO: 'AUTO',
+};
+
+export const AUTO_PLAY_DIR = {
+  STL: 'SMALL_TO_LARGE',
+  LTS: 'LARGE_TO_SMALL',
+};
+
+export default {
+  LIVEPHOTO_DEFAULT,
+  PLAY_MODE,
+  AUTO_PLAY_DIR,
+};
