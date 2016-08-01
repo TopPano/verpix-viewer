e0c846b5a20a59f25a7875e37d0e76b115bb6607
SuJiaKuan <jiakuan.su@toppano.in>
2016-08-01 18:18:54 +0800

    Enable view-only mode for web page

--- demo/main.js
+++ demo/main.js
@@ -1,22 +1,67 @@
-function handleClickBtn(e) {
+import queryString from 'query-string';
+
+const addedPostIds = [];
+
+function handleClickGenBtn(e) {
   e.preventDefault();
-  const postIdInput = document.getElementById('post-id');
-  const postId = postIdInput.value;
+  const querys = queryString.stringify({
+    viewOnly: 'on',
+    postIds: addedPostIds.join(','),
+  });
+  const url = `${window.location.href}?${querys}`;
+  window.open(url);
+}
+
+function enableGenBtn() {
+  const genBtn = document.getElementById('gen-btn');
+  genBtn.disabled = false;
+  genBtn.style.backgroundColor = 'red';
+  genBtn.addEventListener('click', handleClickGenBtn);
+}
+
+function addPost(postId) {
+  const label = document.createElement('H3');
   const wrapper = document.createElement('DIV');
-  wrapper.setAttribute('data-id', postId);
   window.verpix.createLivephoto(wrapper, postId, (err) => {
-    const msgBox = document.getElementById('msg');
+    const msgBox = document.getElementById('add-msg');
     if (err) {
       msgBox.value = err;
     } else {
+      if (addedPostIds.length === 0) {
+        enableGenBtn();
+      }
+      addedPostIds.push(postId);
+      label.innerHTML = `${addedPostIds.length}. ${postId}`;
+      wrapper.setAttribute('data-id', postId);
+      document.body.appendChild(label);
       document.body.appendChild(wrapper);
       msgBox.value = `Post "${postId}" is added`;
     }
-    postIdInput.value = '';
   });
 }
 
+function handleClickAddBtn(e) {
+  e.preventDefault();
+  const postIdInput = document.getElementById('add-id');
+  const postId = postIdInput.value;
+  addPost(postId);
+  postIdInput.value = '';
+}
+
 window.addEventListener('load', () => {
-  const button = document.getElementById('gen-post');
-  button.addEventListener('click', handleClickBtn);
+  const querys = queryString.parse(location.search);
+  if (querys.viewOnly === 'on') {
+    // Non-editable mode
+    if (querys.postIds) {
+      const postIds = querys.postIds.split(',');
+      postIds.forEach((postId) => {
+        addPost(postId);
+      });
+    }
+  } else {
+    // Editable mode
+    document.getElementById('add-form').style.display = 'block';
+    const button = document.getElementById('add-btn');
+    button.addEventListener('click', handleClickAddBtn);
+  }
 });
