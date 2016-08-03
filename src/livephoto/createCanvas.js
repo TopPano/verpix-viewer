5edb87ea541ae6e2ff100dde2db024ebc46c52be
SuJiaKuan <jiakuan.su@toppano.in>
2016-08-03 12:42:12 +0800

    RWD for livephoto

--- src/livephoto/createCanvas.js
+++ src/livephoto/createCanvas.js
@@ -1,29 +1,44 @@
-export default function createCanvas(root, canvasDimension, wrapperDimension) {
-  const wrapper = document.createElement('DIV');
-  const canvas = document.createElement('CANVAS');
-  const canvasStyleWidth = wrapperDimension.width;
-  const canvasStyleHeight =
-    Math.round(canvasDimension.height * (wrapperDimension.width / canvasDimension.width));
+/* eslint-disable no-param-reassign */
 
-  // Styles for wrapper
-  wrapper.style.width = `${wrapperDimension.width}px`;
-  wrapper.style.height = `${wrapperDimension.height}px`;
-  wrapper.style.position = 'relative';
-  wrapper.style.overflow = 'hidden';
+export default function createCanvas(root, canvasDimension, wrapperDimension) {
+  const outWrapper = document.createElement('DIV');
+  const inWrapper = document.createElement('DIV');
+  const canvas = document.createElement('CANVAS');
+  const wrapperRatio = Math.round((wrapperDimension.height / wrapperDimension.width) * 100);
+  const canvasRatio = Math.round((canvasDimension.height / canvasDimension.width) * 100);
+
+  // TODO: How to pass the no-param-reassign rule from eslint ?
+  root.style.width = `${wrapperDimension.width}px`;
+  root.style.maxWidth = '100%';
+
+  // Styles for outter wrapper
+  outWrapper.style.width = '100%';
+  outWrapper.style.paddingBottom = `${wrapperRatio}%`;
+  outWrapper.style.position = 'relative';
+  outWrapper.style.overflow = 'hidden';
+
+  // Styles for inner wrapper
+  inWrapper.style.width = '100%';
+  inWrapper.style.paddingBottom = `${canvasRatio}%`;
+  inWrapper.style.position = 'absolute';
+  inWrapper.style.top = '50%';
+  inWrapper.style.left = '0';
+  inWrapper.style.transform = 'translateY(-50%)';
 
   // Attributes for canvas
   canvas.width = canvasDimension.width;
   canvas.height = canvasDimension.height;
   // styles for canvas
-  canvas.style.width = `${canvasStyleWidth}px`;
-  canvas.style.height = `${canvasStyleHeight}px`;
+  canvas.style.width = '100%';
+  canvas.style.height = '100%';
   canvas.style.cursor = 'pointer';
   canvas.style.position = 'absolute';
-  canvas.style.top = '50%';
-  canvas.style.marginTop = `-${Math.round(canvasStyleHeight / 2)}px`;
+  canvas.style.top = '0';
+  canvas.style.left = '0';
 
-  wrapper.appendChild(canvas);
-  root.appendChild(wrapper);
+  root.appendChild(outWrapper);
+  outWrapper.appendChild(inWrapper);
+  inWrapper.appendChild(canvas);
 
   return canvas;
 }
