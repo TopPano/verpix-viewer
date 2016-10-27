3e29ce4ca0bda5fad2d3a68f48022817316a2df9
SuJiaKuan <jiakuan.su@toppano.in>
2016-10-27 15:24:24 +0800

    Enable dynamically applying CDN URL

--- src/api/livephoto/constructPhotoUrls.js
+++ src/api/livephoto/constructPhotoUrls.js
@@ -1,11 +1,13 @@
 import range from 'lodash/range';
 
 export default function constructPhotoUrls(mediaId, content, quality) {
-  const { count, shardingKey } = content;
-  // TODO: Get cdnUrl from content
-  const cdnUrl = 'http://52.198.24.135:6559';
+  const {
+    cdnUrl,
+    count,
+    shardingKey,
+  } = content;
 
   return range(0, count).map((idx) => (
-    `${cdnUrl}/${shardingKey}/media/${mediaId}/live/${quality}/${idx}.jpg`
+    `${cdnUrl}${shardingKey}/media/${mediaId}/live/${quality}/${idx}.jpg`
   ));
 }
