cc684222106fb64091cb82dd6ce366bff4250d28
SuJiaKuan <jiakuan.su@toppano.in>
2017-03-20 19:01:38 +0800

    Enable mockup mobile devices in testing

--- src/lib/devices/isMobile.js
+++ src/lib/devices/isMobile.js
@@ -1,5 +1,11 @@
 import MobileDetect from 'mobile-detect';
 
 export default function isMobile() {
-  return Boolean(new MobileDetect(window.navigator.userAgent).mobile());
+  const isMo =  Boolean(new MobileDetect(window.navigator.userAgent).mobile());
+
+  if (isMo) {
+    return true;
+  } else {
+    return false;
+  }
 }
--- test/helpers.js
+++ test/helpers.js
@@ -1,4 +1,13 @@
+/* eslint-disable max-len */
+
 import MobileDetect from 'mobile-detect';
+
+const IOS = 'ios';
+const ANDROID = 'android';
+const USER_AGENTS = {
+  [IOS]: 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_2_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13D15 Safari/601.1',
+  [ANDROID]: 'Mozilla/5.0 (Linux; Android 6.0.1; ASUS_Z00UD Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Mobile Safari/537.36',
+};
 
 export function getOS() {
   return new MobileDetect(window.navigator.userAgent).os();
@@ -20,10 +29,32 @@ export function inIframe() {
   return window.self !== window.top;
 }
 
+function setUserAgent(userAgent) {
+  const originalNavigator = window.navigator;
+
+  window.navigator = {};
+  window.navigator.__proto__ = originalNavigator; // eslint-disable-line no-proto
+  window.navigator.__defineGetter__('userAgent', () => userAgent); // eslint-disable-line no-underscore-dangle
+}
+
+export function mockMobile(os = IOS) {
+  const originalUserAgent = window.navigator.userAgent;
+  const mockedOS = USER_AGENTS[os] ? USER_AGENTS[os] : originalUserAgent;
+
+  setUserAgent(mockedOS);
+
+  return {
+    restore: () => {
+      setUserAgent(originalUserAgent);
+    },
+  };
+}
+
 export default {
   getOS,
   inIOS,
   inAndroid,
   inMobile,
   inIframe,
+  mockMobile,
 };
