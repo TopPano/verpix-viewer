5c067b3835e16a2a525393653cc36fba5f73a21e
SuJiaKuan <jiakuan.su@toppano.in>
2017-05-08 17:31:54 +0800

    Modify method of generating signature
    The original signature generating method is time costing. To make
    performance better, we removed the pbkdf2-hashed part and only
    used bcrypt-hashed part.

--- src/api/common/getMedia.js
+++ src/api/common/getMedia.js
@@ -2,7 +2,6 @@ import fetch from 'isomorphic-fetch';
 import isString from 'lodash/isString';
 import Promise from 'bluebird';
 import bcrypt from 'bcryptjs';
-import pbkdf2 from 'pbkdf2';
 
 import config from 'config';
 
@@ -12,14 +11,9 @@ const FETCH_ROOT = config.fetchRoot;
 const AUTH_MESSAGE_PREFIX = 'VERPIX ';
 // Parameters of bcrypt
 const BCRYPT_SALT_ROUND = 5;
-// Parameters of pbkdf2
-const PBKDF2_ITERS = 20;
-const PBKDF2_KEY_LENGTH = 32;
-const PBKDF2_DIGEST = 'sha512';
 // Promisify the async functions
 const genBcryptSalt = Promise.promisify(bcrypt.genSalt);
 const bcryptHash = Promise.promisify(bcrypt.hash);
-const pbkdf2Hash = Promise.promisify(pbkdf2.pbkdf2);
 
 const reverseString = (str) => str.split('').reverse().join('');
 
@@ -39,45 +33,19 @@ export default function getMedia(mediaId, quality, apiKey) {
   const date = new Date();
   // Get current timestamp for authentication message
   const timestamp = parseInt(date.getTime() / 1000, 10);
-  // Determine the plain text to bcrypt-hash and pbkdf2-hash,
-  // and the format depends on the value of timestamp:
-  // Timestamp is odd:  'bcrypt($timestamp + $apiKey) + pbkdf2($resourceName + $timestamp)'
-  // Timestamp is even: 'bcrypt($timestamp + reverse($resourceName)) + pbkdf2($apiKey + $timestamp)'
+  // Signature format depends on the value of timestamp:
+  // Timestamp is odd:  'bcrypt($timestamp + $apiKey + $resourceName + reverse($timestamp))'
+  // Timestamp is even: 'bcrypt($timestamp + reverse($resourceName) + $apiKey + $timestamp)'
   const isOdd = timestamp & 1;
-  const bcryptPlain =
+  const plain =
     isOdd ?
-    `${timestamp}${apiKey}` :
-    `${timestamp}${reverseString(resourceName)}`;
-  const pbkdf2Plain =
-    isOdd ?
-    `${resourceName}${timestamp}` :
-    `${apiKey}${timestamp}`;
-  // The bcrypt-hashed part
-  let bcryptHashed = '';
-  // The pbkdf2-hashed part
-  let pbkdf2Hashed = '';
+    `${timestamp}${apiKey}${resourceName}${reverseString(timestamp.toString())}` :
+    `${timestamp}${reverseString(resourceName)}${apiKey}${timestamp}`;
 
   return genBcryptSalt((BCRYPT_SALT_ROUND))
     // Bcrypt-Hash
-    .then((salt) => bcryptHash(bcryptPlain, salt))
-    .then((hashed) => {
-      bcryptHashed = hashed;
-
-      // PBKDF2-Hash
-      return pbkdf2Hash(
-        pbkdf2Plain,
-        `${timestamp}`,
-        PBKDF2_ITERS,
-        PBKDF2_KEY_LENGTH,
-        PBKDF2_DIGEST
-      );
-    })
-    .then((hashed) => {
-      // The return pbkdf2-hashed value is Uint8Array, we convert it to hexadecimal string
-      pbkdf2Hashed = hashed.toString('hex');
-
-      // Construct the signature by combining bcrypt-hashed part and pbkdf2-hashed part
-      const signature = `${bcryptHashed}${pbkdf2Hashed}`;
+    .then((salt) => bcryptHash(plain, salt))
+    .then((signature) => {
       // Construct the authentication message.
       // The foramt is: 'VERPIX $apiKey:$signature'
       const authMessage = `${AUTH_MESSAGE_PREFIX}${apiKey}:${signature}`;
