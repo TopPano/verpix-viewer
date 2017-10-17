db02f1bfb3f64d44f3c48de2e36b188b3973c76b
SuJiaKuan <jiakuan.su@toppano.in>
2017-10-17 14:33:31 +0800

    Modify demo page
    In this commit, we modified demo page to make it more powerful.
    The developers can input media ID, width, height and type when
    adding a new media for testing.

--- demo/main.js
+++ demo/main.js
@@ -2,15 +2,18 @@
 
 import queryString from 'query-string';
 
-const addedPostIds = [];
+const mediaList = [];
 
 function handleClickGenBtn(e) {
   e.preventDefault();
+
   const querys = queryString.stringify({
     viewOnly: 'on',
-    postIds: addedPostIds.join(','),
+    mediaList: mediaList.map((media) =>
+      `${media.id},${media.type},${media.width},${media.height}`).join(';'),
   });
   const url = `${window.location.href}?${querys}`;
+
   window.open(url);
 }
 
@@ -21,25 +24,33 @@ function enableGenBtn() {
   genBtn.addEventListener('click', handleClickGenBtn);
 }
 
-function addPost(postId) {
+function addMedia(id, type, width, height) {
   const label = document.createElement('H3');
   const params = {
+    width,
+    height,
   };
+  const method = (type === 'panophoto') ? 'createPanophoto' : 'createLivephoto';
 
-  window.verpix.createLivephoto(postId, params, (err, instance) => {
+  window.verpix[method](id, params, (err, instance) => {
     const msgBox = document.getElementById('add-msg');
     if (err) {
       msgBox.value = err;
     } else {
-      if (addedPostIds.length === 0) {
+      if (mediaList.length === 0) {
         enableGenBtn();
       }
-      addedPostIds.push(postId);
-      label.innerHTML = `${addedPostIds.length}. ${postId}`;
+      mediaList.push({
+        id,
+        type,
+        width,
+        height,
+      });
+      label.innerHTML = `${mediaList.length}. ${id} (${type} ${width}x${height})`;
       instance.root.style.border = 'dashed 1px grey';
       document.body.appendChild(label);
       document.body.appendChild(instance.root);
-      msgBox.value = `Post "${postId}" is added`;
+      msgBox.value = `Media "${id}" is added`;
       instance.start();
     }
   });
@@ -47,20 +58,27 @@ function addPost(postId) {
 
 function handleClickAddBtn(e) {
   e.preventDefault();
-  const postIdInput = document.getElementById('add-id');
-  const postId = postIdInput.value;
-  addPost(postId);
-  postIdInput.value = '';
+
+  const idInput = document.getElementById('add-id');
+  const typeSelect = document.getElementById('add-type');
+  const id = idInput.value;
+  const type = typeSelect.options[typeSelect.selectedIndex].value;
+  const width = parseInt(document.getElementById('add-width').value, 10);
+  const height = parseInt(document.getElementById('add-height').value, 10);
+
+  addMedia(id, type, width, height);
+  idInput.value = '';
 }
 
 window.addEventListener('load', () => {
   const querys = queryString.parse(location.search);
   if (querys.viewOnly === 'on') {
     // Non-editable mode
-    if (querys.postIds) {
-      const postIds = querys.postIds.split(',');
-      postIds.forEach((postId) => {
-        addPost(postId);
+    if (querys.mediaList) {
+      const mediaInfoList = querys.mediaList.split(';');
+      mediaInfoList.forEach((mediaInfo) => {
+        const info = mediaInfo.split(',');
+        addMedia(info[0], info[1], info[2], info[3]);
       });
     }
   } else {
